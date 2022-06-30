63bd5dbae7d22af397c11a16e875b82792765768
Sheva393 <sheva961@gmail.com>
2022-06-30 22:29:28 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
 
 const App = () => {
-  return (
-    <div>
-
-    </div>
-  );
+  return <div className="app"></div>;
 };
 
 export default App;
